b7fa6da82e686a56089d46e0d3b8ff118e134837
aakash <spysky1998@gmail.com>
2021-05-23 04:49:57 -0700

    aa

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/data", { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://bakwas:bakwas@cluster0.uvmz9.mongodb.net/userData?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log("Connection successfull..."))
 .catch((err)=> console.log(err))
 
--- DB/schema.js
+++ DB/schema.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const buyerData = new mongoose.Schema({
+const data = new mongoose.Schema({
     account_id:String,
     amount: Number,
     order_id:String,
     created_at:Number
 });
 
-const Data = new mongoose.model('All_data', buyerData)
+const Data = new mongoose.model('All_data', data)
 
 module.exports = Data;

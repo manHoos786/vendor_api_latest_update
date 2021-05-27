8cc83a2b8e7814a098c514a4f054376aa3353e03
aakash <spysky1998@gmail.com>
2021-05-27 09:57:50 -0700

    aaka

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-
 mongoose.connect("mongodb+srv://bakwas:bakwas@cluster0.uvmz9.mongodb.net/bank_unique_id?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> console.log("Connection successfull..."))
 .catch((err)=> console.log(err))
 // HeyIWillFuckYou
+// https://polar-lake-69736.herokuapp.com
 

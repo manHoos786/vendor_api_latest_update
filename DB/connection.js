cce9ee9ef108b0cd1d584ccf6bf09afe8c2d1806
aakash <spysky1998@gmail.com>
2022-06-28 13:20:15 -0700

    Latest version

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://bakwas:bakwas@cluster0.uvmz9.mongodb.net/bank_unique_id?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> console.log("Connection successfull..."))
+mongoose.connect("mongodb+srv://bakwas:bakwas@cluster0.uvmz9.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> console.log("Database connected successfully..."))
 .catch((err)=> console.log(err))
 // HeyIWillFuckYou
 // https://polar-lake-69736.herokuapp.com
